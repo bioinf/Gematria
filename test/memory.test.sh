bb357254ca27e45301cfa7006caeb6d63a296f83
Igor V <re.latur@gmail.com>
2018-09-22 03:35:39 +0300

    Bash Memory-tester

---
+++ test/memory.test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -f ./memory_usage
+$@ &
+pid=$!
+trap "kill $pid 2> /dev/null" EXIT
+while kill -0 $pid 2> /dev/null; do
+    cat /proc/$pid/status | grep "VmHWM" | awk {'print $2'} >> ./memory_usage
+    sleep 1
+done
+trap - EXIT
+echo $(cat ./memory_usage)
