5483a4c027b44def3e86756d5d6c81b6df62a1f1
Igor V <re.latur@gmail.com>
2018-11-11 02:19:02 +0300

    Memtest updated

--- test/memory.test.sh
+++ test/memory.test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 $@ &
 pid=$!
+echo "Command: $@" >> ./memory_usage_$pid
 trap "kill $pid 2> /dev/null" EXIT
 while kill -0 $pid 2> /dev/null; do
     cat /proc/$pid/status | grep "VmHWM" | awk {'print $2'} >> ./memory_usage_$pid
