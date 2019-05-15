0c350650c627e63d80d332c1dbc469afb407dff9
Igor V <re.latur@gmail.com>
2019-05-15 12:48:52 +0300

    Directory tree update

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ setup(
     name = 'makegms',
     version = '3.0.2',
     description = 'Fast and easy tool for genome mappability estimation',
-    url = "https://github.com/latur/MakeGMS",
+    url = "https://github.com/latur/Gematria",
     author = 'Igor V.',
     author_email = 'latur@me.com',
     ext_modules = [Extension(
       'makegms', 
-      ['makegms/src/makegms.c'], 
+      ['makegms/makegms.c'], 
       include_dirs = ['src'],
       extra_compile_args=['-std=c99', '-m64', '-O3'] # '-lpthread'
     )],
     py_modules = ['makegms']
 )
 
-# rm -rf makegms* dist build
+# rm -rf makegms.* dist build
 # python3 setup.py build
 # python3 setup.py install
